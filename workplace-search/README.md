09a2619c25de08f8221b40d4a3343fb4a74b022f
tamros <tamara@elastic.co>
2021-01-25 10:50:31 +0100

    Workplace Search Workshop python client
    Insert demo data inside Workplace Search using Elastic Enterprise Search python client

---
+++ workplace-search/README.md
@@ -0,0 +1,37 @@
+# wpsearch_workshop
+
+wpsearch.py is a helper script to load some sample data into elastic work place search that's intended to be used for workshops.
+
+requirements: python 3.8, elastic_workplace_search python client
+
+usage: wpsearch.py [-h] [-p] {employees,trello,issues}
+
+- Create an enterprise search deployment in cloud.elastic.co
+- Create 3 custom api sources named employees, issues and trello
+- Clone repo
+- Copy endpoint, access_token and keys for individual sources and paste into sections in config.yaml
+- Install Requirements
+
+`python -m pip install elastic-enterprise-search`
+- Load data one source at a time using the script
+
+`python wpsearch.py employees`
+
+
+`python wpsearch.py issues`
+
+
+`python wpsearch.py trello`
+
+- When done, optionally purge data
+
+`python wpsearch.py employees -p`
+
+
+`python wpsearch.py issues -p`
+
+
+`python wpsearch.py trello -p`
+
+
+
---
+++ workplace-search/wpsearch.py
@@ -0,0 +1,45 @@
+import argparse
+import json
+import yaml
+from elastic_enterprise_search import WorkplaceSearch
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source', help='source name',
+                        choices={'employees','issues','trello'})
+    parser.add_argument('-p', '--purge', action='store_true',
+                        help='purge all data for the source')
+    return parser.parse_args()
+
+def get_config(filename='config.yaml'):
+    with open(filename) as f:
+        return yaml.safe_load(f)
+
+def get_data(filename):
+    with open(filename) as f:
+        return [json.loads(line) for line in f]
+
+def upload_data():
+    data = get_data(source['file'])    
+    client.index_documents(content_source_key=key,body=data)
+
+def purge_data():
+    data = get_data(source['file'])
+    ids = [str(i) for i in range(1,len(data)+1)]
+    client.delete_documents(key, ids)
+
+args = parse_args()
+config = get_config()
+deployment =config['deployment']
+source = get_config()[args.source]
+key = source['key']
+base_url = deployment['endpoint']
+client = WorkplaceSearch(base_url, http_auth=deployment['access_token'])
+if args.purge:
+    purge_data()
+else:
+    upload_data()
+
+
+
